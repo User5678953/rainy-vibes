f229002f5348df3f08817810fcef0d863adab4d8
User5678953 <kendynwebster@gmail.com>
2025-06-05 18:51:56 -0500

    chore: add Netlify config

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,19 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      
       </div>
-      <h1>Vite + React</h1>
+      <h1>Howdy Jordan</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+         Fromage Loves you!
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        So does ozzy
       </p>
     </>
   )
